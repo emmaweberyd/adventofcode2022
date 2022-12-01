9709556e7f1d83c8c5dcd77af4a38ece1e2bfd82
emmaweberyd <emma.weberyd@gmail.com>
2022-12-01 17:08:42 +0100

    upload day 1 part 2

---
+++ 1/2/README.md
@@ -0,0 +1,7 @@
+# Day 1: Calorie Counting - Part Two
+
+From a terminal within this directory, run: 
+
+```bash
+python3 solution.py
+```
---
+++ 1/2/solution.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        lines = f.readlines()
+
+    calories_per_elf = []
+    current_sum = 0
+
+    for line in lines:
+        if line == '\n':
+            calories_per_elf.append(current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(line.replace('\n',''))
+
+    calories_per_elf.sort(reverse=True)
+
+    sum_of_top_three_amounts_of_calories = sum(calories_per_elf[:3])
+    
+    print('The 3 elves with the most calories have a total of {} calories'.format(sum_of_top_three_amounts_of_calories))
