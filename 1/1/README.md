b09030d27f2018831fb93a77ec109d5d27e15401
emmaweberyd <emma.weberyd@gmail.com>
2022-12-01 16:29:47 +0100

    upload day 1 part 1

---
+++ 1/1/README.md
@@ -0,0 +1,7 @@
+# Day 1: Calorie Counting - Part One
+
+From a terminal within this directory, run: 
+
+```bash
+python3 solution.py
+```
---
+++ 1/1/solution.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        lines = f.readlines()
+
+    max_cal = 0
+    current_sum = 0
+
+    for line in lines:
+        if line == '\n':
+            max_cal = max(max_cal, current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(line.replace('\n',''))
+    
+    print('The elf with the most calories has {} calories'.format(max_cal))
